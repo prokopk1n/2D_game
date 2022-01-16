aa8af7ec05848c2299f48d38c2aae0aaef8917d1
Prokopkin Sergey <71408915+prokopk1n@users.noreply.github.com>
2022-01-16 17:11:39 +0300

    Add files via upload

---
+++ src/Image.h
@@ -0,0 +1,55 @@
+#ifndef MAIN_IMAGE_H
+#define MAIN_IMAGE_H
+
+#include <string>
+#include <GLFW/glfw3.h>
+
+constexpr int tileSize = 32;
+
+struct Pixel
+{
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+  uint8_t a;
+};
+
+
+constexpr Pixel backgroundColor{0, 0, 0, 0};
+
+struct Image
+{
+  explicit Image(const std::string &a_path);
+  Image(int a_width, int a_height, int a_channels);
+  Image(const Image & obj);
+  const Image& operator=(const Image & obj);
+
+  int Save(const std::string &a_path);
+
+  int & Width()    { return width; }
+  int & Height()   { return height; }
+  int Channels() const { return channels; }
+  size_t Size()  const { return size; }
+  Pixel* Data()        { return  data; }
+  void PutImage(int x,int y,Image & picture,bool clear=false);
+  void PutImageDirect(int x,int y,Image & picture,bool clear=false);
+
+  Pixel GetPixel(int x, int y) { return data[width * y + x];}
+  void  PutPixel(int x, int y, const Pixel &pix) { data[width* y + x] = pix; }
+
+  ~Image();
+
+  Image(){};
+
+private:
+  int width = -1;
+  int height = -1;
+  int channels = 3;
+  size_t size = 0;
+  Pixel *data = nullptr;
+  bool self_allocated = false;
+};
+
+Pixel darker(Pixel oldpix, Pixel newpix);
+
+#endif //MAIN_IMAGE_H
